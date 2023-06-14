9294022f2d941787d1ec86ea4912dcf5edc33bd7
Julian Mesa <djnetswa@gmail.com>
2023-06-14 00:44:23 -0500

    set webhook

--- src/app/api/messages/route.js
+++ src/app/api/messages/route.js
@@ -15,14 +15,18 @@ export async function GET(request) {
   let message = "";
 
   const bot = new Telegraf(process.env.BOT_TOKEN);
-  await bot.telegram.setWebhook("https://hunty-bot.vercel.app/api/messages");
 
-  await bot.telegram.setMyCommands([
+  //await bot.telegram.setWebhook("https://hunty-bot.vercel.app/api/messages");
+
+  //return NextResponse.json({ message: "Webhook setted" });
+
+  /*await bot.telegram.setMyCommands([
     { command: "/start", description: "Iniciar el bot" },
     { command: "/help", description: "Solicitar ayuda" },
-  ]);
+  ]);*/
 
   bot.start((ctx) => ctx.reply("ℹ️ Bienvenidos al bot de Hunty!"));
+
   bot.help((ctx) =>
     ctx.reply("ℹ️ Escribe el nombre de un departamento (Ejemplo: Antioquia)")
   );
@@ -71,53 +75,4 @@ export async function GET(request) {
   const messages = await prisma.message.findMany();
 
   return NextResponse.json({ messages });
-}
-
-export async function POST(request) {
-  const res = await fetch("https://api-colombia.com/api/v1/Department", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data = await res.json();
-
-  const bot = new Telegraf(process.env.BOT_TOKEN);
-  bot.telegram.setWebhook("https://hunty-bot.vercel.app/api/messages");
-
-  bot.telegram.setMyCommands([
-    { command: "/start", description: "Iniciar el bot" },
-    { command: "/help", description: "Solicitar ayuda" },
-  ]);
-
-  bot.start((ctx) => ctx.reply("ℹ️ Bienvenidos al bot de Hunty!"));
-  bot.help((ctx) =>
-    ctx.reply("ℹ️ Escribe el nombre de un departamento (Ejemplo: Antioquia)")
-  );
-  bot.on("text", (ctx) => {
-    const depto = ctx.message.text;
-
-    const result = data.find((row) => row.name === depto);
-
-    if (!result) {
-      bot.telegram.sendMessage(
-        ctx.message.chat.id,
-        `❌ No se encontraron resultados para el departamento *${depto}*, verifica que esté bien escrito.`
-      );
-      bot.telegram.sendMessage(
-        ctx.message.chat.id,
-        `ℹ️ Si necesitas ayuda puedes solicitarla escribiendo /help.`
-      );
-    } else {
-      bot.telegram.sendMessage(
-        ctx.message.chat.id,
-        `✅ Excelente, aquí tienes la información de ${result.name}:`
-      );
-
-      bot.telegram.sendMessage(ctx.message.chat.id, result.description);
-    }
-  });
-
-  bot.launch();
-
-  return NextResponse.json({ message: "ok" });
 }
