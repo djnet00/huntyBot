98844c2259918300e9cbbb4fd19f7af9c26c5bce
Julian Mesa <djnetswa@gmail.com>
2023-06-14 01:19:47 -0500

    add setup

---
+++ src/app/api/setup/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+const { Telegraf } = require("telegraf");
+
+export async function GET(request) {
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+
+  bot.telegram.setWebhook("https://hunty-bot.vercel.app/api/webhook");
+
+  return NextResponse.json({ message: "Webhook setted" });
+}
