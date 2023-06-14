24c27e7c7f983d0e1dac1f369db9ac2127f2f57b
Julian Mesa <djnetswa@gmail.com>
2023-06-14 00:55:26 -0500

    fix message

--- src/app/api/messages/route.js
+++ src/app/api/messages/route.js
@@ -16,7 +16,7 @@ export async function GET(request) {
 
   const bot = new Telegraf(process.env.BOT_TOKEN);
 
-  //await bot.telegram.setWebhook("https://hunty-bot.vercel.app/api/messages");
+  bot.telegram.setWebhook("https://hunty-bot.vercel.app/api/messages");
 
   //return NextResponse.json({ message: "Webhook setted" });
 
@@ -31,46 +31,52 @@ export async function GET(request) {
     ctx.reply("ℹ️ Escribe el nombre de un departamento (Ejemplo: Antioquia)")
   );
   bot.on("text", async (ctx) => {
-    const depto = ctx.message.text;
+    if (ctx.message.chat.id > 0) {
+      const depto = ctx.message.text;
 
-    await prisma.message.create({
-      data: {
-        username: ctx.from.username,
-        messageId: ctx.message.message_id,
-        chatId: ctx.message.chat.id,
-        message: depto,
-        type: "USER",
-      },
-    });
+      await prisma.message.create({
+        data: {
+          username: ctx.from.username,
+          messageId: ctx.message.message_id,
+          chatId: ctx.message.chat.id,
+          message: depto,
+          type: "USER",
+        },
+      });
 
-    const result = await data.find((row) => row.name === depto);
+      const result = data.find((row) => row.name === depto);
 
-    if (!result) {
-      messageText = `❌ No se encontraron resultados para el departamento *${depto}*, verifica que esté bien escrito.`;
-      bot.telegram.sendMessage(ctx.message.chat.id, message);
-      bot.telegram.sendMessage(
-        ctx.message.chat.id,
-        `ℹ️ Si necesitas ayuda puedes solicitarla escribiendo /help.`
-      );
-    } else {
-      messageText = `✅ Excelente, aquí tienes la información de ${result.name}:`;
-      await bot.telegram.sendMessage(ctx.message.chat.id, message);
+      if (!result) {
+        messageText = `❌ No se encontraron resultados para el departamento *${depto}*, verifica que esté bien escrito.`;
+        bot.telegram.sendMessage(ctx.message.chat.id, message);
+        bot.telegram.sendMessage(
+          ctx.message.chat.id,
+          `ℹ️ Si necesitas ayuda puedes solicitarla escribiendo /help.`
+        );
+      } else {
+        messageText = `✅ Excelente, aquí tienes la información de ${result.name}:`;
+        bot.telegram.sendMessage(ctx.message.chat.id, message);
 
-      await bot.telegram.sendMessage(ctx.message.chat.id, result.description);
+        bot.telegram.sendMessage(ctx.message.chat.id, result.description);
+      }
+
+      await prisma.message.create({
+        data: {
+          username: ctx.from.username,
+          messageId: ctx.message.message_id,
+          chatId: ctx.message.chat.id,
+          message: messageText,
+          type: "BOT",
+        },
+      });
     }
-
-    await prisma.message.create({
-      data: {
-        username: ctx.from.username,
-        messageId: ctx.message.message_id,
-        chatId: ctx.message.chat.id,
-        message: messageText,
-        type: "BOT",
-      },
-    });
   });
 
-  await bot.launch();
+  bot.launch();
+
+  // Enable graceful stop
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
   const messages = await prisma.message.findMany();
 
